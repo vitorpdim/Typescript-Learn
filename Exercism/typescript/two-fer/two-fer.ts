fb0f0f9cb30e0873fe0d4fb1fb87fe9c888d944c
vito <vitor.cynh@gmail.com>
2026-03-14 15:59:34 -0300

    feat: exercism #1

--- Exercism/typescript/two-fer/two-fer.ts
+++ Exercism/typescript/two-fer/two-fer.ts
@@ -1,3 +1,11 @@
-export function twoFer(name: string): string {
-  return name
+export function twoFer(name?: string): string {
+  if(name === undefined){
+    return "One for you, one for me."
+  }else{
+  return `One for ${name}, one for me.`
+  }
 }
+
+console.log(twoFer())
+console.log(twoFer("Alice"))
+console.log(twoFer("Do-Yun"))
