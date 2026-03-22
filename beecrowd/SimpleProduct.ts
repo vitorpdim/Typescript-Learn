1ec1be47064806486d51f1b2af16703a95a63d4a
vito <vitor.cynh@gmail.com>
2026-03-22 15:32:51 -0300

    feat(beecrowd) #3

--- beecrowd/SimpleProduct.ts
+++ beecrowd/SimpleProduct.ts
@@ -4,8 +4,8 @@ const input = fs.readFileSync('/dev/stdin', 'utf-8')
 const lines = input.split("\n");
 
 function Prod(){
-    const A = input(lines[0])
-    const B = input(lines[1])
+    const A = parseInt(lines[0])
+    const B = parseInt(lines[1])
 
     const prod = A * B
     return `PROD = ${prod}`
