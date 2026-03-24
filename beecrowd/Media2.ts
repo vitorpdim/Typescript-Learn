c150ec4bd864c6da58485d718074b6171ef910b8
vito <vitor.cynh@gmail.com>
2026-03-23 22:23:22 -0300

    feat(beecrowd) #5

--- beecrowd/Media2.ts
+++ beecrowd/Media2.ts
@@ -1,15 +1,15 @@
 import * as fs from "fs"
 
 const input = fs.readFileSync("/dev/stdin", "utf-8")
-const lines = input.split(/\s+/)
+const lines = input.trim().split(/\s+/)
 
 const pesoA = 2.0
 const pesoB = 3.0
 const pesoC = 5.0
 
-const A = parseFloat(lines([0]))
-const B = parseFloat(lines([1]))
-const C = parseFloat(lines([2]))
+const A = parseFloat(lines[0])
+const B = parseFloat(lines[1])
+const C = parseFloat(lines[2])
 
 const media = (A * pesoA + B * pesoB + C * pesoC) / 10
 
