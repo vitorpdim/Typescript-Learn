29295bb471d2bc283648c9e8b5eeb191aabfac4f
vito <vitor.cynh@gmail.com>
2026-03-23 21:31:54 -0300

    feat(beecrowd) #4

---
+++ beecrowd/Media1.ts
@@ -0,0 +1,11 @@
+import * as fs from "fs"
+
+const input = fs.readFileSync("/dev/stdin", "utf-8")
+const lines = input.trim().split("\n")
+
+const A = parseFloat(lines)
+const B = parseFloat(lines)
+
+const MEDIA = (A * 3.5 + B * 7.5) / 11.0
+
+console.log(`MEDIA = ${MEDIA.toFixed(5)}`)
