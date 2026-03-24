fa531e5c5e55b0e14d0652c585f197cac9784fca
vito <vitor.cynh@gmail.com>
2026-03-23 22:17:47 -0300

    feat(beecrowd) #5

--- beecrowd/Media1.ts
+++ beecrowd/Media1.ts
@@ -3,9 +3,8 @@ import * as fs from "fs"
 const input = fs.readFileSync("/dev/stdin", "utf-8")
 const lines = input.trim().split("\n")
 
-const A = parseFloat(lines?.trim() || "0")
-const B = parseFloat(lines?.trim() || "0")
-
+const A = parseFloat(lines[0])
+const B = parseFloat(lines[1])
 const MEDIA = (A * 3.5 + B * 7.5) / 11.0
 
 console.log(`MEDIA = ${MEDIA.toFixed(5)}`)
---
+++ beecrowd/Media2.ts
@@ -0,0 +1,16 @@
+import * as fs from "fs"
+
+const input = fs.readFileSync("/dev/stdin", "utf-8")
+const lines = input.split(/\s+/)
+
+const pesoA = 2.0
+const pesoB = 3.0
+const pesoC = 5.0
+
+const A = parseFloat(lines([0]))
+const B = parseFloat(lines([1]))
+const C = parseFloat(lines([2]))
+
+const media = (A * pesoA + B * pesoB + C * pesoC) / 10
+
+console.log(`MEDIA = ${media.toFixed(1)}`)
